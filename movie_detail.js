d94e99b7a2160a9dc292fc761ed28a0f9a9e0f7a
Eleanor Bucke <107644138+eleanorbucke21@users.noreply.github.com>
2024-02-23 11:18:15 +0000

    videos added to json. filter display buttons

--- movie_detail.js
+++ movie_detail.js
@@ -1,26 +1,37 @@
 const urlParams = new URLSearchParams(window.location.search);
 const movieId = urlParams.get('id');
 
+// Function to extract YouTube video ID from the URL
+function getYouTubeVideoId(url) {
+    const videoIdMatch = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+    return videoIdMatch ? videoIdMatch[1] : null;
+}
+
 // Fetch the film data from the JSON file
 fetch('film.json')
     .then(response => response.json())
     .then(data => {
-        
         const movies = data.movies;
-
         const selectedMovie = movies.find(movie => movie.id == movieId);
 
         // Check if the movie is found
         if (selectedMovie) {
-           
             document.getElementById('movieImage').src = selectedMovie.posterUrl;
             document.getElementById('movieTitle').textContent = selectedMovie.title;
             document.getElementById('moviePlot').textContent = selectedMovie.plot;
             document.getElementById('movieCast').textContent = `Cast: ${selectedMovie.actors}`;
             document.getElementById('movieDirector').textContent = `Director: ${selectedMovie.director}`;
             document.getElementById('movieYear').textContent = `Released: ${selectedMovie.year}`;
+
+            const videoFrame = document.getElementById("movieVideo");
+
+            // Extract YouTube video ID
+            const videoId = getYouTubeVideoId(selectedMovie.videoUrl);
+
+            // Embed YouTube video using the video ID
+            videoFrame.src = `https://www.youtube.com/embed/${videoId}`;
         } else {
-            // Error if not found
+            // Error
             console.error(`Movie with ID ${movieId} not found.`);
         }
     })
